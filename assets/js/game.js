1b4b7f14bea9620ab7b861d2e22fd96ca441078a
Ben-Boyte <zissette1@gmail.com>
2022-04-25 14:35:46 -0500

    added if/else statements about attacking

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,6 +21,15 @@ var fight = function() {
 console.log(
   playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 );
+
+if (enemyHealth <= 0) {
+  window.alert(enemyName = " has died!");
+}
+
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
   playerHealth = playerHealth - enemyAttack;
 
@@ -28,6 +37,13 @@ console.log(
   console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
+
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
 };
 
 fight();
