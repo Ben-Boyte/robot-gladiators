170cbdb82a32b003f713da67b4519419c94b7736
Ben-Boyte <zissette1@gmail.com>
2022-04-26 13:52:34 -0500

    deleted s from Names and added loop at bottom, removed console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,11 +9,8 @@ console.log(playerName, playerAttack, playerHealth);
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length);
+
+
 
 for(var i = 0; i < enemyNames.length; i++) {
   console.log(enemyNames[i]);
@@ -23,7 +20,7 @@ for(var i = 0; i < enemyNames.length; i++) {
 
 
 
-var fight = function() {
+var fight = function(enemyName) {
  
   window.alert("Welcome to Robot Gladiators!");
 
@@ -34,21 +31,21 @@ var fight = function() {
   enemyHealth = enemyHealth - playerAttack;
 
 console.log(
-  playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 );
 
 if (enemyHealth <= 0) {
-  window.alert(enemyNames = " has died!");
+  window.alert(enemyName = " has died!");
 }
 
 else {
-  window.alert(enemyNames + " still has " + enemyHealth + " health left.");
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
 }
 
   playerHealth = playerHealth - enemyAttack;
 
   console.log(
-    enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
 
   if (playerHealth <= 0) {
@@ -78,4 +75,6 @@ else {
 }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
